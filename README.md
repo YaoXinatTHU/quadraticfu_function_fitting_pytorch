884c99870019bafc1e2ad392c379ece65f7afba0
Yao Xin <gzyaoxin@corp.netease.com>
2018-01-11 19:17:49 +0800

    update readme

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ ReLU+ ReLU
 ![image](https://github.com/YaoXinatTHU/quadraticfu_function_fitting_pytorch/blob/master/images_folder/relu.png)
 
 Tanh + Tanh
-![image](https://github.com/YaoXinatTHU/quadraticfu_function_fitting_pytorch/blob/master/images_folder/tank*2.png)
+![image](https://github.com/YaoXinatTHU/quadraticfu_function_fitting_pytorch/blob/master/images_folder/tanh*2.png)
 
 Tanh + ReLU
 ![image](https://github.com/YaoXinatTHU/quadraticfu_function_fitting_pytorch/blob/master/images_folder/tanh+relu.png)
@@ -24,3 +24,19 @@ Sigmoid + Tanh
 Sigmoid + Sigmoid
 ![image](https://github.com/YaoXinatTHU/quadraticfu_function_fitting_pytorch/blob/master/images_folder/sigmoid*2.png)
 
+蓝色的点是真实函数中采样出来的，红色的线是神经网络拟合出来的结果。
+
+很容易看到的是：
+
+1、在训练集的区域内[-1,1]，除了sigmoid + sigmoid 激活函数组合外，其他都拟合得很好。
+
+2、在训练集的区间外几乎无一例外的GG了。
+
+3、sigmoid + sigmoid的激活函数组合直接失败了，跑了好几次基本都是这个结果，所以应该不是网络的初始值的问题。
+
+从这个有限规模的实验上来看，有如下结论：
+1、“神经网络有理论上能拟合任何函数的的能力”在数据空间比较小的时候是没问题的（数据范围比较大的时候等main2.py的分析）。但是这个拟合其实是很盲目的，参见2。
+
+2、而在训练数据看不见的地方（[-1,1]以外的区域），模型就表现得辣眼睛了。这应该是因为神经网络拟合出来的函数其实并不是`$y=x^2+1$`本身，而是在训练集的区域内，用了很多模型能表示的分段函数去近似它。（很明显的是relu+relu的模型中，拟合出来的线其实是由几条直线构成的），这种拟合其实有点记住训练数据的意思，泛化能力不好是正常的。毕竟在数据量有限的情况下，模型不太好预测真实数据的分布（就[-1,1]区间的数据，从`$y=x^2+1$`采样的数据和从`$y=|x|+1$`采样的数据并没有太大差别）。随后会扩大数据范围看看模型的表现。目测只用很多分段函数来近似的方法都不会好。
+
+3、sigmoid + sigmoid的函数表现不好的原因暂时没想到，照例来说它和tanh应该差不多。目前猜测是因为sigmoid的取值范围只在[0,1]之间吧？但是为什么拟合出的总是一条直线(？或者是近似直线？)这个还是没想清楚。
---
+++ main2.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+import torch.utils.data
+import torch.optim as optim
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.nn = nn.Sequential(
+            nn.Linear(1,50),
+            nn.Tanh(),
+            nn.Linear(50, 100),
+            nn.Tanh(),
+            nn.Linear(100, 20),
+            nn.Tanh(),
+            nn.Linear(20,1),
+        )
+    def forward(self, input):
+        return self.nn(input)
+
+if __name__ == '__main__':
+    n = Net()
+    print(n)
+    input = torch.unsqueeze(torch.linspace(-10, 10, 100), dim=1)
+    output = torch.pow(input,2) + 1
+    ds = torch.utils.data.TensorDataset(input,output)
+    loader = torch.utils.data.DataLoader(ds,batch_size=20, shuffle=True)
+    optimizer = optim.SGD(n.parameters(), lr=0.001)
+    loss = torch.nn.MSELoss()
+    for i in range(100):
+        for batch_idx, (data, target) in enumerate(loader):
+            data, target = Variable(data), Variable(target)
+            out = n(data)
+            optimizer.zero_grad()
+            l = loss(out,target)
+            l.backward()
+            optimizer.step()
+        out = n(Variable(input,requires_grad = False))
+        if i % 5 == 0:
+            plt.cla()
+            plt.scatter(input.numpy(), output.numpy())
+            plt.plot(input.numpy(), out.data.numpy(),'r-')
+            plt.text(-0.75, 2, 'Round=%d,Loss=%.4f' % (i,l.data[0]), fontdict={'size': 20, 'color':  'red'})
+            plt.pause(0.3)
+    plt.close()
+    input2 = torch.unsqueeze(torch.linspace(-20, 20, 200), dim=1)
+    out_real = torch.pow(input2,2) + 1
+    out2 = n(Variable(input2)).data
+    plt.scatter(input2.numpy(), out_real.numpy())
+    plt.plot(input2.numpy(), out2.numpy(), 'r-')
+    plt.text(-3, 16, 'Round=%d,Loss=%.4f' % (100, l.data[0]), fontdict={'size': 20, 'color': 'red'})
+    plt.show()
